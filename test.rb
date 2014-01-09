87de4105fe9426161804d082049da20362fe6be5
Britney Wright <blw06g@gmail.com>
2014-01-09 13:04:56 -0500

    fixed LoanDebt tests

--- test.rb
+++ test.rb
@@ -13,23 +13,21 @@ describe "StudentLoan" do
 	end
 end
 
-describe "LoanDebt" do	
-	before :all do
-		@loan = [
+describe LoanDebt do	
+	before do
+		loans = [
 		 StudentLoan.new("loan_a", 100, 6.5, 50),
-		 StudentLoan.new("loan_a", 200, 6.0, 50)
+		 StudentLoan.new("loan_b", 200, 6.0, 50)
 		]
-	end
 
-	before :each do
-		@loan_debt = LoanDebt.new(@loan)
+		@loan_debt = LoanDebt.new(*loans)
 	end	
 
 	it "should total all student loan balances" do
-		@loan_debt.loan_total.must_equal 100
+		@loan_debt.loan_total.must_equal 300
 	end
 
 	it "total all student loans interest" do
-		@loan_debt.interest_total.must_equal 6.5
+		@loan_debt.interest_total.must_equal 18.5
 	end	
 end
